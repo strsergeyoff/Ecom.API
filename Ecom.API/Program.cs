68f3a4af2ad370fc6dbc106568712df39cbc59ef
strsergeyoff <strsergeyoff@gmail.com>
2024-08-15 17:25:01 +0300

    Изменениея

--- Ecom.API/Program.cs
+++ Ecom.API/Program.cs
@@ -200,8 +200,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
-//app.UseAuthorization();
+app.UseHttpsRedirection();
+app.UseAuthorization();
 
 app.MapControllers();
 
